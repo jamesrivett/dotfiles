dbf31759f5b76286a2814cb4851ce6e0749a5578
jamesrivett <jamesrivett77@gmail.com>
2024-04-24 15:27:06 -0400

    trying different source method

--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -8,6 +8,7 @@ VIM_CONF_URL="https://raw.githubusercontent.com/jamesrivett/dotfiles/master/.vim
 VIM_CONF_TARGET=$HOME/.vimrc
 
 curl -L $BASH_CONF_URL > $BASH_CONF_TARGET && source $BASH_CONF_TARGET
+curl -L $BASH_CONF_URL | source
 curl -L $TMUX_CONF_URL > $TMUX_CONF_TARGET && tmux source-file $TMUX_CONF_TARGET
 curl -L $VIM_CONF_URL > $VIM_CONF_TARGET
 
