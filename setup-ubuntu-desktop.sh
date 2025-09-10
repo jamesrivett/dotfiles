abfee4deb5ecbc02d105f3b9e09364bb0bf3fc04
jamesrivett <jamesrivett77@gmail.com>
2025-09-09 22:13:13 -0400

    getting started

--- setup-ubuntu-desktop.sh
+++ setup-ubuntu-desktop.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
+DEBIAN_FRONTEND=noninteractive
 
+# Update Ubuntu Sources
+sudo apt-get update
+
+# Install apt packages
+sudo apt-get install -y \ 
+	tmux curl vim
+
+# Install Snaps
+sudo snap install discord 
+
+# Copy some configs from dotfiles repo
