a8202335138c8f80136b0908d15e794f509d34a1
jamesrivett <jamesrivett77@gmail.com>
2025-09-09 23:36:37 -0400

    pushing up for the night

--- setup-ubuntu-desktop.sh
+++ setup-ubuntu-desktop.sh
@@ -1,15 +1,36 @@
 #!/bin/bash
 
+# Resolve script directory (src: https://stackoverflow.com/a/246128)
+SOURCE_PATH="${BASH_SOURCE[0]}"
+# Resolve symlinks recursively
+while [ -L "$SOURCE_PATH" ]; do
+# Get symlink directory
+SYMLINK_DIR="$( cd -P "$( dirname "$SOURCE_PATH" )" >/dev/null 2>&1 && pwd )"
+# Resolve symlink target (relative or absolute)
+SOURCE_PATH="$(readlink "$SOURCE_PATH")"
+# Check if candidate path is relative or absolute
+if [[ $SOURCE_PATH != /* ]]; then
+	# Candidate path is relative, resolve to full path
+	SOURCE_PATH=$SYMLINK_DIR/$SOURCE_PATH
+fi
+done
+# Get final script directory path from fully resolved source path
+SCRIPT_DIR="$(cd -P "$( dirname "$SOURCE_PATH" )" >/dev/null 2>&1 && pwd)"
+echo "Current Directory (should be dotfiles repo): $SCRIPT_DIR"
+
+$SCRIPT_DIR/
+
 DEBIAN_FRONTEND=noninteractive
 
 # Update Ubuntu Sources
-sudo apt-get update
+apt-get update
 
 # Install apt packages
-sudo apt-get install -y \ 
-	tmux curl vim
+apt-get install -y \ 
+	tmux curl vim git
 
 # Install Snaps
-sudo snap install discord 
+snap install discord 
 
-# Copy some configs from dotfiles repo
+# Run the dotfiles installer
+
